40e33bd3d508a397c40bee0c1b952d0383929fb5
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-01-27 17:50:15 +0900

    수정 : 쿼리파라미터 @RequestParam 으로 처리

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -6,10 +6,8 @@ import com.example.todo.dto.request.TodoDeleteRequestDto;
 import com.example.todo.dto.request.TodoUpdateRequestDto;
 import com.example.todo.dto.response.ResponseTodoDto;
 import com.example.todo.service.TodoService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,20 +25,9 @@ public class TodoController {
     }
 
     @GetMapping("")
-    public ResponseEntity<ResponseDto<ResponseTodoDto>> findAll(HttpServletRequest request){
-        String userId = request.getParameter("userId");
-        String page = request.getParameter("page");
-
-        Long userIdValue = null;
-        Long pageValue = 1L;
-        if(userId != null && !userId.equals("null")){
-            userIdValue = Long.parseLong(userId);
-        }
-        if(page != null && !page.equals("null")){
-            pageValue = Long.parseLong(page);
-        }
-        List<ResponseTodoDto> all = todoService.findAll(userIdValue,pageValue);
-        return ResponseEntity.ok(new ResponseDto<ResponseTodoDto>(all,"success"));
+    public ResponseEntity<ResponseDto<ResponseTodoDto>> findAll(@RequestParam(value = "userId", required = false) Long userId, @RequestParam(value = "page", defaultValue = "0") Long page){
+        List<ResponseTodoDto> all = todoService.findAll(userId,page);
+        return ResponseEntity.ok(new ResponseDto<>(all,"success"));
     }
 
     @GetMapping("/{id}")
@@ -56,13 +43,13 @@ public class TodoController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<ResponseDto<ResponseTodoDto>> update(@PathVariable Long id, @RequestBody @Valid TodoUpdateRequestDto updateDto){
+    public ResponseEntity<ResponseDto<Void>> update(@PathVariable Long id, @RequestBody @Valid TodoUpdateRequestDto updateDto){
         int updated = todoService.update(id, updateDto);
         return ResponseEntity.ok(new ResponseDto<>("success"));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDto<ResponseTodoDto>> delete(@PathVariable Long id, @RequestBody @Valid TodoDeleteRequestDto deleteDto){
+    public ResponseEntity<ResponseDto<Void>> delete(@PathVariable Long id, @RequestBody @Valid TodoDeleteRequestDto deleteDto){
         int deleted = todoService.deleteById(id,deleteDto);
         return ResponseEntity.ok(new ResponseDto<>("success"));
     }
